173e6431f064a04e5e8a8e746f4b94e7a7ed70db
Nandini Singh <singhnandini60810@gmail.com>
2026-03-16 15:38:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-document.getElementById("loginForm").addEventListener("submit",function(e){
+document.getElementById("loginForm").addEventListener("submit", function(e){
 
 e.preventDefault();
 
-let username=document.querySelector("input[type=text]").value;
-let password=document.querySelector("input[type=password]").value;
+let username = document.querySelector("input[type=text]").value;
+let password = document.querySelector("input[type=password]").value;
+let message = document.getElementById("message");
 
-if(username==="admin" && password==="1234"){
-document.getElementById("message").style.color="green";
-document.getElementById("message").textContent="Login Successful!";
+if(username === "admin" && password === "1234"){
+message.style.color="lightgreen";
+message.textContent="Login Successful!";
 }
 else{
-document.getElementById("message").textContent="Invalid username or password";
+message.style.color="red";
+message.textContent="Invalid Login Details";
 }
 
 });
